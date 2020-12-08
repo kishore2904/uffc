3b991eb71b98e8bc6e0730de328f2c74038fe3d4
Kishore <47620468+kishore2904@users.noreply.github.com>
2020-12-08 22:57:00 +0530

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,3 +1,18 @@
 <?php
-    include_once("index.html");
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $messgae = $_POST['message'];
+
+    $email_from = 'ckishore168@gmail.com';
+
+    $email_subject = "New submission";
+
+    $email_body = "User_name: $name.\n".
+                    "User_email: $visitor_email.\n".
+                    "User message: $messgae.\n";
+    $to = "kishore290499@gmail.com";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: index.html")
 ?>
